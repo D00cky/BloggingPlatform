579e5c2e9e00d86fa437125328d4b4acec07d0b0
d00 <83293500+D00cky@users.noreply.github.com>
2025-12-15 10:25:27 -0300

    Create the makeAPost function in the service

--- src/main/java/com/Api/BloggingPlatform/service/UserAndPostService.java
+++ src/main/java/com/Api/BloggingPlatform/service/UserAndPostService.java
@@ -1,33 +1,31 @@
 package com.Api.BloggingPlatform.service;
 
 import com.Api.BloggingPlatform.model.UserModel;
+import com.Api.BloggingPlatform.model.PostModel;
+import com.Api.BloggingPlatform.repository.PostRepository;
 import com.Api.BloggingPlatform.repository.UserRepository;
-import jakarta.annotation.Nullable;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserAndPostService {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
-    public ResponseEntity<List<UserModel>> getAllPosts(String title) {
-        List<UserModel> userpost = new ArrayList<UserModel>();
+    public UserAndPostService(UserRepository userRepo, PostRepository postRepo){
+        this.userRepository = userRepo;
+        this.postRepository = postRepo;
+    }
 
-        if (title == null)
-            userRepository.findAll().forEach(userpost::add);
-        else
-            userRepository.findByTitleContaining(title).forEach(userpost::add);
-        if (userpost.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-    return new ResponseEntity<>(userpost, HttpStatus.OK);
+    public PostModel makeAPost(String author, String title, String content) {
+        PostModel newPost = new PostModel();
+        newPost.setTitle(title);
+        newPost.setContent(content);
+        newPost.setAuthor(author);
+        return postRepository.save(newPost);
     }
 }
