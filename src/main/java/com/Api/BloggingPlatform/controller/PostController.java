ab6b8774eb18315388c0bf75b273a177f80c5ac2
d00 <83293500+D00cky@users.noreply.github.com>
2025-12-21 15:31:56 -0300

    Created Classes from post and user for controller and service, Sucessfully created a new post and get the author from the post.

--- src/main/java/com/Api/BloggingPlatform/controller/PostController.java
+++ src/main/java/com/Api/BloggingPlatform/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.Api.BloggingPlatform.controller;
 
 import com.Api.BloggingPlatform.model.PostModel;
+import com.Api.BloggingPlatform.repository.PostRepository;
 import com.Api.BloggingPlatform.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/blog")
 public class PostController {
 
-    @Autowired
-    PostService postService;
+    private PostService postService;
+    private PostRepository postRepository;
+
+    public PostController(PostService postService, PostRepository postRepository) {
+        this.postService = postService;
+        this.postRepository = postRepository;
+    }
 
     @PostMapping("/post/new")
     public ResponseEntity<Object> createPost(@RequestBody PostModel postModel) {
